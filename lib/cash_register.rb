5175621fc9d503b5a421c61329136ed95ef29e03
Valeria  Copleman <valeriacopleman@gmail.com>
2020-09-21 04:06:21 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,7 +1,7 @@
 class CashRegister
   attr_accessor :cash_register, :cash_register_with_discount
   
-  def initialize(total=0, cash_register_with_discount)
+  def initialize(cash_register_with_discount, total=0)
     @total = total
     @cash_register_with_discount = cash_register_with_discount
   end

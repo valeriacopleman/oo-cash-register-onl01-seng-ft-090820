ecd5d0371a0e6fc8b433a096674ef4c1d882692d
Valeria  Copleman <valeriacopleman@gmail.com>
2020-09-21 03:55:20 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,7 @@
-class 
+class CashRegister
+  attr_accessor :cash_register, :cash_register_with_discount
+  
+  def initialize(cash_register, cash_register_with_discount)
+    @cash_register
+    @cash_register_with_discount
+  end
